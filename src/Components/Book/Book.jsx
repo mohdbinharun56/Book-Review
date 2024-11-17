63dd63f9ccedb21310be8e15b5275bab9011b12b
mohdbinharun56 <arahaman461@gmail.com>
2024-11-17 11:04:12 +0600

    implementing the books card section

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types'; 
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
 const Book = ({book}) => {
     console.log(book);
     const { bookName,category,image,rating,tags,author} = book;
     return (
-        <div className="card bg-base-100 shadow-xl mb-10">
+        <div className="card bg-base-100 shadow-xl mb-10 cursor-pointer">
             <figure className='bg-base-200 rounded-xl m-10 mb-2 p-5'>
                 <img
                     src={image}
@@ -24,7 +25,7 @@ const Book = ({book}) => {
                 <hr/>
                 <div className="card-actions justify-between mt-5">
                     <div className="badge text-[#767575] font-sans font-medium">{category}</div>
-                    <div className="badge text-[#767575] font-sans font-medium">{rating}</div>
+                    <div className="badge text-[#767575] font-sans font-medium"> <span className='mr-2'>{rating}</span><span><FontAwesomeIcon icon={faStar} /></span></div>
                 </div>
             </div>
         </div>
