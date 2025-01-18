c3eb69d26bb51e0b4f38be328c7386a94b706dd3
mohdbinharun56 <arahaman461@gmail.com>
2025-01-18 11:58:45 +0600

    implementing the sort-by functional in descending order

--- src/Pages/ListedBooks/ListedBooks.jsx
+++ src/Pages/ListedBooks/ListedBooks.jsx
@@ -6,26 +6,23 @@ import BooksList from "../../Components/BooksList/BooksList";
 const ListedBooks = () => {
     const [listBooks, setListBooks] = useState([]);
     const [booksData, setBooksData] = useState([]);
-    const [books, setBooks] = useState([]);
-    
+    const [sortedBooks, setSortedBooks] = useState([]);
+
     const data = useLoaderData();
     const clickRead = useRef();
-    useEffect(()=>{
+
+    useEffect(() => {
         clickRead.current.click();
-    },[]);
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() => {
         setBooksData(data);
-    },[data]);
-    
-    useEffect(()=>{
-        const exist = booksData.filter(book=> listBooks.includes(book.bookId));
-        if(exist.length>0){
-            setBooks(exist);
-        }
-    },[booksData,listBooks])
+    }, [data]);
 
-    // console.log(books);
+    useEffect(() => {
+        const exist = booksData.filter(book => listBooks.includes(book.bookId));
+        setSortedBooks(exist);
+    }, [booksData, listBooks]);
 
     const handleList = (key) => {
         if (key === "Read") {
@@ -46,36 +43,49 @@ const ListedBooks = () => {
         }
     }
 
+    const handleSortChange = (e) => {
+        const sortField = e.target.value;
+
+        let sorted = [...sortedBooks];
+        if (sortField === "Rating") {
+            sorted.sort((a, b) => b.rating - a.rating);
+        } else if (sortField === "number-of-pages") {
+            sorted.sort((a, b) => b.totalPages - a.totalPages);
+        } else if (sortField === "publisher-year") {
+            sorted.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+        }
+        setSortedBooks(sorted);
+    };
+
     return (
         <div>
             <h1 className="text-center font-bold text-2xl bg-base-200 rounded-md mt-10 p-10">Books</h1>
 
             <div className="mt-10 flex justify-center">
-                <select className="text-white p-2 outline-none border border-green-500 text-center rounded-lg bg-green-500 font-semibold text-xl">
-                    <option value="">Sort-By</option>
-                    <option value="">category-1</option>
-                    <option value="">category-2</option>
+                <select
+                    onChange={handleSortChange}
+                    className="text-white p-2 outline-none text-center rounded-lg bg-green-500 font-semibold text-xl"
+                >
+                    <option value="" className=" bg-gray-200 text-black">Sort-By</option>
+                    <option value="Rating" className=" bg-gray-200 text-black">Rating</option>
+                    <option value="number-of-pages" className=" bg-gray-200 text-black">Number of pages</option>
+                    <option value="publisher-year" className=" bg-gray-200 text-black">Publisher year</option>
                 </select>
             </div>
 
             <div className="border-b">
-                <button ref={clickRead} onClick={() => handleList("Read")} className="mr-2 border p-2">Read Bookd</button>
-                <button onClick={() => handleList("Wishlist")} className="mr-2 border p-2">Wishlist</button>
+                <button ref={clickRead} onClick={() => handleList("Read")} className="mr-2 border p-2">
+                    Read Books
+                </button>
+                <button onClick={() => handleList("Wishlist")} className="mr-2 border p-2">
+                    Wishlist
+                </button>
             </div>
 
-            {/* <div>{listBooks.length}</div> */}
-            {/* <div>
-                {
-                    listBooks.length === 0 ?
-                        <p className="text-3xl mt-5 text-center">The Books List is Empty. Please Read or add Wishlist First.</p> :
-                        listBooks.map(id => <p key={id}>{id}</p>)
-                        // booksData.filter(book=>book.bookId === ))
-                }
-            </div> */}
             <div>
-               {
-                    books.map(book=><BooksList key={book.bookId} book={book}></BooksList>) 
-               }
+                {sortedBooks.map(book => (
+                    <BooksList key={book.bookId} book={book}></BooksList>
+                ))}
             </div>
         </div>
     );
